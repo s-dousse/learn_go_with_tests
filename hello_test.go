f3b0d0830b0ad6e748306c9420130a3c6de0f72a
Sarah <dousse.sarah@gmail.com>
2022-02-28 15:35:58 +0000

    add subtest with scenario 'saying hello to people'

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Sarah")
-	want := "Hello, Sarah"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Sarah")
+		want := "Hello, Sarah"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
