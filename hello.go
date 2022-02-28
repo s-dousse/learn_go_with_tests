7c74e1bfd65104d6249f056673a8ef3c88a9f6f0
Sarah <dousse.sarah@gmail.com>
2022-02-28 18:14:07 +0000

    refactoring

--- hello.go
+++ hello.go
@@ -10,7 +10,10 @@ func Hello(name, language string ) string {
 	if name == "" {
 		name = "World"	
 	}
-	var prefix string
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 		case "Spanish":
 			prefix = holaPrefix
@@ -20,7 +23,7 @@ func Hello(name, language string ) string {
 		default:
 			prefix = helloPrefix
 	}
-	 return prefix + name
+	 return
 }
 
 func main() {
