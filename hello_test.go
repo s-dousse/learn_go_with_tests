742b164ce435e281c36ef28e9f2541e4172f2333
Sarah <dousse.sarah@gmail.com>
2022-02-28 15:40:00 +0000

    add subtest with scenario 'say 'Hello, World' when an empty string is supplied'

--- hello_test.go
+++ hello_test.go
@@ -11,4 +11,12 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
