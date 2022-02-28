135698c634ec499400294832f6e943e4ddd48a68
Sarah <dousse.sarah@gmail.com>
2022-02-28 15:48:19 +0000

    refactoring subtests, add helper function assertCorrectMessage

--- hello_test.go
+++ hello_test.go
@@ -3,12 +3,19 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Sarah")
 		want := "Hello, Sarah"
 
 		if got != want {
-			t.Errorf("got %q want %q", got, want)
+		 assertCorrectMessage(t, got, want)
 		}
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
@@ -16,7 +23,7 @@ func TestHello(t *testing.T) {
 		want := "Hello, World"
 
 		if got != want {
-			t.Errorf("got %q want %q", got, want)
+			assertCorrectMessage(t, got, want)
 		}
 	})
 }
