6722235c64b3a64d7dcc71f70c84040e9657dcbc
Sarah <dousse.sarah@gmail.com>
2022-03-01 15:24:25 +0000

    tdd Repeat function

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -8,7 +8,7 @@ func TestAdder(t *testing.T) {
 	expected := 4
 
 	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
+		t.Errorf("expected %d but got %d", expected, sum)
 	}
 }
 
